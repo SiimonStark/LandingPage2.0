e058df2b1c9ed9b2e02949dec8cf48b8dd15a4fb
SiimonStark <justinpyktel@gmail.com>
2019-09-03 15:17:36 -0400

    Setup template for Contact component

---
+++ src/Contact/Contact.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className="overlay">
+      <section className = "Contact">
+        <h3>Phone:</h3>
+        <p>(540)287-9664</p>
+        <h3>Email:</h3>
+        <p>SiimonStark@gmail.com</p>
+        <h3>Github:</h3>
+        <p>Example</p>
+        <h3>LinkedIn:</h3>
+        <p>Example</p>
+        <h3>Twitter:</h3>
+        <p>Example</p>
+      </section>
+    </div>
+  )
+}
+
+export default Contact;
