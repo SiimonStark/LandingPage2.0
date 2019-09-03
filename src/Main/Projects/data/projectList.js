ff6322d88772309e444951e8f8486b08e1c27406
SiimonStark <justinpyktel@gmail.com>
2019-09-03 14:35:39 -0400

    Design format for Projects List

---
+++ src/Main/Projects/data/projectList.js
@@ -0,0 +1,35 @@
+export const projects = [
+  {
+    name: 'Test',
+    description: 'Lorem ipsum, some sort of description for the project at hand. Which prolly shouldnt be longer than a paragraph. Because then it might be getting too long for the alotted space. Therefore needing to be shorted down to a reasonable length. So that it fits in the space given. But it also needs to be detailed enough that the idea of the project gets across to the user.',
+    screenshots: [
+      'primary.com/gif',
+      'secondary.com/img'
+    ],
+    github: 'github.com/example',
+    liveSite: 'example.com',
+    tools: [
+      'React', 'Jest', 'Redux'
+    ]
+  },
+  {
+    name: 'Test2',
+    description: 'Lorem ipsum, some sort of description for the project at hand. Which prolly shouldnt be longer than a paragraph. Therefore needing to be shorted down to a reasonable length. So that it fits in the space given. But it also needs to be detailed enough that the idea of the project gets across to the user.',
+    screenshots: [
+      'primary.com/gif',
+      'secondary.com/img',
+      'secondary.com/img',
+      'secondary.com/img'
+    ],
+    github: 'github.com/example',
+    liveSite: null,
+    tools: [
+      'Vue', 'Testing', 'Framework'
+    ]
+  }
+]
+
+export const tools = {
+  React: 'example',
+  Redux: 'example2'
+}
