5d6158a1d796d12f6bc658e20986ed8a04e80542
SiimonStark <justinpyktel@gmail.com>
2019-11-02 11:06:29 -0400

    Hadn't worked on in awhile

--- src/Main/Projects/Projects.js
+++ src/Main/Projects/Projects.js
@@ -1,17 +1,47 @@
 import React, { Component } from 'react';
+import Primary from './Primary/Primary';
+import Mini from './Mini/Mini';
 
 class Projects extends Component {
   constructor() {
     super()
     this.state = {
+      user: {},
       projects: [],
     }
+  }
+
+  componentDidMount() {
+    const url = 'https://api.github.com/users/siimonstark';
+    fetch(url)
+      .then(response => response.json())
+      .then(data => this.compileData(data))
+      .then(user => this.setState({user}))
+      .catch(error => console.log('**failed to fetch: ', error))
+  }
+
+  compileData = (user) => {
+    const userInfo = {name: user.name, image: user.avatar_url};
+    console.log(user);
+    const repos = this.gatherStarred(user.starred_url);
+    return userInfo;
+  }
+
+  gatherStarred = (star_url) => {
+    return fetch("https://api.github.com/users/SiimonStark/starred")
+      .then(response => response.json())
+      .then(data => console.log('Star Repos: ', data))
   }
 
   render() {
     return (
       <section className="Projects">
-        Projects
+        <section className="projects-container">
+          <Primary />
+          <div className="projects_other">
+            <Mini />
+          </div>
+        </section>
       </section>
     )
   }
