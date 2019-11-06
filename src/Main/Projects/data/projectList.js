45349a57d79e9eaebf6094ea788412fd6b91a223
SiimonStark <justinpyktel@gmail.com>
2019-11-06 11:25:00 -0500

    Filled out data for projects

--- src/Main/Projects/data/projectList.js
+++ src/Main/Projects/data/projectList.js
@@ -1,35 +1,75 @@
 export const projects = [
   {
-    name: 'Test',
-    description: 'Lorem ipsum, some sort of description for the project at hand. Which prolly shouldnt be longer than a paragraph. Because then it might be getting too long for the alotted space. Therefore needing to be shorted down to a reasonable length. So that it fits in the space given. But it also needs to be detailed enough that the idea of the project gets across to the user.',
+    name: 'GameCave',
+    description: "GameCave is a React web application that allows a user to search through a database of videogames and/or randomly generate a suggested game to play.",
     screenshots: [
-      'primary.com/gif',
-      'secondary.com/img'
+      'https://user-images.githubusercontent.com/23123990/53147491-f41c2e00-3564-11e9-855a-c4bb272f36a0.gif',
+      'https://user-images.githubusercontent.com/23123990/53151649-f97f7580-3570-11e9-90f8-62d905cce6cd.gif'
     ],
-    github: 'github.com/example',
-    liveSite: 'example.com',
+    github: 'https://github.com/SiimonStark/GameCave',
+    liveSite: null,
     tools: [
-      'React', 'Jest', 'Redux'
+      'React', 'Enzyme', 'Jest'
     ]
   },
   {
-    name: 'Test2',
-    description: 'Lorem ipsum, some sort of description for the project at hand. Which prolly shouldnt be longer than a paragraph. Therefore needing to be shorted down to a reasonable length. So that it fits in the space given. But it also needs to be detailed enough that the idea of the project gets across to the user.',
+    name: 'Lexicon',
+    description: "Can't find the word that's on the tip of your tongue? In Lexicon we provide users with a welcoming UI/UX and is MADE for Mobile devices, so you can quickly find the words you're looking for.",
     screenshots: [
-      'primary.com/gif',
-      'secondary.com/img',
-      'secondary.com/img',
-      'secondary.com/img'
+      'https://camo.githubusercontent.com/2473a22936d6fe831dcbee87a3eb8be3823a6f8e/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f59336553356a724c56694c3849494c3356662f67697068792e676966',
+      'https://user-images.githubusercontent.com/23123990/62233646-348a9e00-b386-11e9-8c8d-8f6c686239e6.png'
     ],
-    github: 'github.com/example',
+    github: 'https://github.com/SiimonStark/Lexicon',
+    liveSite: 'https://lexicon-thesaurus.herokuapp.com/',
+    tools: [
+      'Vue', 'Vuetify', 'SCSS'
+    ]
+  },
+  {
+    name: 'ButterFlix',
+    description: "Ever see a movie come out in theaters, yet you don't have the time to see it during it's release?Welcome to Butterflix, where you can 'Track your Movies'! See the latest releases, updated through the MovieDatabase API. Favorite, add to wishlist, or track movies.",
+    screenshots: [
+      'https://camo.githubusercontent.com/b3ef80f6de7d9f8fc8ff5b8418cefd05b47c1c06/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f645a583336717179564632697453577a79692f67697068792e676966',
+      'https://user-images.githubusercontent.com/23123990/57737623-8d8d4480-7669-11e9-8951-96403ccc2e33.png',
+      'https://user-images.githubusercontent.com/23123990/57737407-da245000-7668-11e9-9fe4-3b0f398a5d0b.png'
+    ],
+    github: 'https://github.com/SiimonStark/ButterFlix',
     liveSite: null,
     tools: [
-      'Vue', 'Testing', 'Framework'
+      'React', 'Router', 'Redux', 'Enzyme', 'Jest', 'SCSS'
+    ]
+  },
+  {
+    name: 'Gametime: Jeopardy',
+    description: "As our first Object Oriented project using Vanilla JS, we created the game of Jeopardy with a Pokemon™️ theme. Professor Oak quizes 3 trainers on their trivia skills.",
+    screenshots: [
+      'https://user-images.githubusercontent.com/23123990/52432862-dbbe0500-2ac8-11e9-8907-3a2168a2ff14.png',
+      'https://user-images.githubusercontent.com/23123990/52432860-db256e80-2ac8-11e9-8c0e-0ca64e1494c4.png',
+      'https://user-images.githubusercontent.com/23123990/52432861-dbbe0500-2ac8-11e9-935d-0f365a964b32.png'
+    ],
+    github: 'https://github.com/SiimonStark/gametime_jeopardy_jp_ro',
+    liveSite: null,
+    tools: [
+      'Vanilla JS', 'JQuery', 'Mocha'
+    ]
+  },
+  {
+    name: 'Wheel of the Wasteland',
+    description: "Fallout themed Wheel of Fortune game. Three player game where each player has a chance to guess the word puzzle, spin the wheel, or buy a vowel to further progress the puzzle.s",
+    screenshots: [
+      'https://github.com/MaxBSilver/wheel-of-the-wasteland/raw/master/src/images/Screen%20Shot%202019-03-28%20at%207.07.07%20AM.png?raw=true',
+      'https://github.com/MaxBSilver/wheel-of-the-wasteland/raw/master/src/images/Screen%20Shot%202019-03-28%20at%207.07.17%20AM.png?raw=true',
+      'https://github.com/MaxBSilver/wheel-of-the-wasteland/raw/master/src/images/Screen%20Shot%202019-03-28%20at%207.08.40%20AM.png?raw=true'
+    ],
+    github: 'https://github.com/MaxBSilver/wheel-of-the-wasteland',
+    liveSite: null,
+    tools: [
+      'Vanilla JS', 'JQuery', 'Mocha'
     ]
   }
 ]
 
-export const tools = {
+export const toolsIcons = {
   React: 'example',
   Redux: 'example2'
 }
